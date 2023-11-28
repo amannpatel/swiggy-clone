02a052330fa2fe42ada26a0b22d1b0407c9419e0
Aman Patel <aman17626@gmail.com>
2023-11-28 20:22:59 +0530

    introduced jsx

--- script.js
+++ script.js
@@ -5,10 +5,14 @@ const parent = React.createElement(
   "div",
   { id: "heading" },
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an H1 tag."),
-    React.createElement("h2", {}, "I'mm an H2 tag."),
+    React.createElement("h1", {}, "I'm an H1 tag.🚀"),
+    React.createElement("h2", {}, "I'mm an H2 tag.✅"),
   ])
 );
 
+const heading = <h1 id="header">I'm also an H1 tag created using JSX!🚀</h1>;
+console.log(heading);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+// root.render(parent);
+root.render(heading);

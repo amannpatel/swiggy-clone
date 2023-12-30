a5cf44c6c4ae4919ea138c499125c7d93b72a753
Aman Patel <aman17626@gmail.com>
2023-12-30 20:45:04 +0530

    Updated the API calls

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,29 @@
-import { useState } from "react";
-import resList from "../utils/mockData";
+import { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [listOfRestaurant, setListOfRestaurant] = useState(resList);
+  const [listOfRestaurant, setListOfRestaurant] = useState([]);
+
+  useEffect(() => {
+    // console.log("Helloo");
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.148619415971503&lng=91.78715276919789&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    // console.log(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+    setListOfRestaurant(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  if (listOfRestaurant.length === 0) {
+    return <Shimmer />;
+  }
 
   return (
     <div className="body">
@@ -12,7 +32,7 @@ const Body = () => {
           className="filter-btn"
           onClick={() => {
             const filteredList = listOfRestaurant.filter((res) => {
-              return res.info.avgRating > 4;
+              return res.info.avgRating > 4.4;
             });
             // console.log(filteredList);
             setListOfRestaurant(filteredList);

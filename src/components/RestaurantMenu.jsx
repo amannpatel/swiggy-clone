790410113ef15985563b14d3736cc688d265d489
Aman Patel <aman17626@gmail.com>
2024-01-12 20:13:32 +0530

    Refactor RestaurantMenu component and custom hook

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -1,33 +1,21 @@
-import { useEffect, useState } from "react";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
-import { MENU_API } from "../utils/constants";
 
 const RestaurantMenu = () => {
-  const [resInfo, setResInfo] = useState(null);
-
   const { resId } = useParams();
 
-  useEffect(() => {
-    fetchMenu();
-  }, []);
-
-  const fetchMenu = async () => {
-    const data = await fetch(MENU_API + resId);
-    const json = await data.json();
-    // console.log(json);
-    setResInfo(json.data);
-  };
+  const resInfo = useRestaurantMenu(resId);
 
   if (resInfo === null) return <Shimmer />;
 
-  const { name, cuisines, cloudinaryImageId, costForTwoMessage } =
-    resInfo?.cards[0]?.card?.card?.info;
+  const { name, cuisines, costForTwoMessage } =
+    resInfo?.data?.cards[0]?.card?.card?.info;
 
-  // Maybe some cards contains cards[2]
+  // Maybe some cards contains data in cards[2]
   const { itemCards } =
-    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
-
+    resInfo?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card
+      ?.card;
   //   console.log(itemCards);
 
   return (

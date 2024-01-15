dd153d0874d27c619f5857d22205f3a066f5e36a
Aman Patel <aman17626@gmail.com>
2024-01-15 21:28:14 +0530

    Refactor component rendering logic

--- src/components/RestaurantCategory.jsx
+++ src/components/RestaurantCategory.jsx
@@ -1,11 +1,22 @@
 import ItemList from "./ItemList";
+import { useState } from "react";
 
 const RestaurantCategory = ({ data }) => {
   //   console.log(data);
+  const [showItems, setShowItems] = useState(false);
+
+  const handleClick = () => {
+    // showItems ? setShowItems(false) : setShowItems(true);
+    setShowItems(!showItems);
+  };
+
   return (
     <div>
       {/* {Header} */}
-      <div className="w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4 hover:cursor-pointer">
+      <div
+        className="w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4 hover:cursor-pointer"
+        onClick={handleClick}
+      >
         <div className="flex justify-between">
           <span className="font-bold text-lg">
             {data.title} ({data.itemCards.length})
@@ -13,7 +24,7 @@ const RestaurantCategory = ({ data }) => {
           <span>🔽</span>
         </div>
         {/* Accordion Body */}
-        <ItemList items={data.itemCards} />
+        {showItems && <ItemList items={data.itemCards} />}
       </div>
     </div>
   );
